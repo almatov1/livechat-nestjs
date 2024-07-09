502cae173099386a13f5cbec1608f1fb200fbc6a
almatov1 <almatovalshin@gmail.com>
2024-07-09 14:23:46 +0500

    handlers

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,21 @@ async function bootstrap() {
   await app.listen(3000);
 }
 bootstrap();
+
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import * as fs from 'fs';
+
+// async function bootstrap() {
+//   const httpsOptions = {
+//     key: fs.readFileSync('nest.key'),
+//     cert: fs.readFileSync('nest.cert'),
+//   };
+
+//   const app = await NestFactory.create(AppModule, {
+//     httpsOptions,
+//   });
+
+//   await app.listen(3002);
+// }
+// bootstrap();
--- src/websocket/websocket.gateway.ts
+++ src/websocket/websocket.gateway.ts
@@ -1,48 +1,31 @@
 import {
+  ConnectedSocket,
+  MessageBody,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
-  OnGatewayConnection,
-  OnGatewayDisconnect,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway(3002)
-export class WebsocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
+@WebSocketGateway({ cors: true })
+export class WebsocketGateway {
   @WebSocketServer() server: Server;
 
-  // Обрабатываем подключение клиента
-  handleConnection(client: Socket) {
-    console.log('Client connected:', client.id);
-  }
-
-  // Обрабатываем отключение клиента
-  handleDisconnect(client: Socket) {
-    console.log('Client disconnected:', client.id);
-  }
-
-  // Обрабатываем сообщение о присоединении к комнате
-  @SubscribeMessage('join')
-  handleJoin(client: Socket, room: string): void {
-    client.join(room); // Подключаем клиента к комнате
-    client.to(room).emit('user-connected', client.id); // Уведомляем остальных пользователей в комнате о новом подключении
-  }
-
-  // Обрабатываем сообщение с предложением (offer) от одного клиента другому
   @SubscribeMessage('offer')
-  handleOffer(client: Socket, payload: { room: string, offer: RTCSessionDescriptionInit }) {
-    client.to(payload.room).emit('offer', payload.offer); // Пересылаем предложение всем в комнате
+  handleOffer(@MessageBody() data: any, @ConnectedSocket() client: Socket): void {
+    console.log('offer by: ', client.id);
+    this.server.emit('offer', data);
   }
 
-  // Обрабатываем сообщение с ответом (answer) на предложение
   @SubscribeMessage('answer')
-  handleAnswer(client: Socket, payload: { room: string, answer: RTCSessionDescriptionInit }) {
-    client.to(payload.room).emit('answer', payload.answer); // Пересылаем ответ всем в комнате
+  handleAnswer(@MessageBody() data: any, @ConnectedSocket() client: Socket): void {
+    console.log('answer by: ', client.id);
+    this.server.emit('answer', data);
   }
 
-  // Обрабатываем сообщение с кандидатом ICE (ICE candidate)
-  @SubscribeMessage('candidate')
-  handleCandidate(client: Socket, payload: { room: string, candidate: RTCIceCandidateInit }) {
-    client.to(payload.room).emit('candidate', payload.candidate); // Пересылаем кандидата всем в комнате
+  @SubscribeMessage('ice-candidate')
+  handleIceCandidate(@MessageBody() data: any, @ConnectedSocket() client: Socket): void {
+    console.log('ice-candidate by: ', client.id);
+    this.server.emit('ice-candidate', data);
   }
 }
